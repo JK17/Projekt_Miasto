de91758289f06c9ac2ddee088f50f12243e9226d
JK17 <jaskoo2@gmail.com>
2019-05-24 18:05:04 +0200

    Add files via upload

---
+++ Miasto.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <vector>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main()
+{
+
+  return 0;
+}
